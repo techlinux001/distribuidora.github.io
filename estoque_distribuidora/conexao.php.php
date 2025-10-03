784d11b4db10663153d148dd7260974f58292c29
techlinux001 <costajulio7771@gmail.com>
2025-10-03 15:55:42 -0300

    Delete estoque_distribuidora directory

--- estoque_distribuidora/conexao.php.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=estoque_db;charset=utf8", "root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die("❌ Erro na conexão com o banco: " . $e->getMessage());
-}
-?>
